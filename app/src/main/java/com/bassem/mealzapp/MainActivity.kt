233a335fb9c422483b37ccf5a7eeb55d65bf7e57
bassem3312 <basem3312@gmail.com>
2023-05-24 19:28:05 +0300

    Commit after apply

--- app/src/main/java/com/bassem/mealzapp/MainActivity.kt
+++ app/src/main/java/com/bassem/mealzapp/MainActivity.kt
@@ -1,11 +1,9 @@
 package com.bassem.mealzapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.viewModels
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.coroutineScope
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.RecyclerView
 import com.bassem.mealzapp.ui.MealsAdapter
